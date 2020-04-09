a56fddd2ef018750130f27d1e9e9be99330ecdd1
lsb <lishoubo2014@126.com>
2020-04-09 22:27:20 +0800

    commit HD1720

--- src/day1_2/HD1720.java
+++ src/day1_2/HD1720.java
@@ -1,4 +1,15 @@
 package day1_2;
-
+import java.util.Scanner;
 public class HD1720 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a,b;
+        while (in.hasNext()){
+            String str = in.nextLine();
+            String[] s = str.split(" ");
+            a = Integer.valueOf(s[0],16);
+            b = Integer.valueOf(s[1],16);
+            System.out.println(a+b);
+        }
+    }
 }
